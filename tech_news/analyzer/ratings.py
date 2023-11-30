95160d8a83f366e39d684c7a51d4a6373ce09f6b
Pedro Medeiros <pedropwks@gmail.com>
2023-11-30 04:03:46 -0300

    Implementando a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,42 @@
-# Requisito 10
+import requests
+from collections import Counter
+# from tech_news.scraper import scrape_news, fetch
+from tech_news.database import search_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    MAIN_URL = "https://blog.betrybe.com/"
+    array = list()
+
+    try:
+        try:
+            # requisition = scrape_news(fetch("https://blog.betrybe.com/"))
+            array = list()
+            MAIN_URL = "https://blog.betrybe.com/"
+            dic = dict()
+            MAIN_URL
+            notices = Counter(
+                notice["category"]
+                for notice in search_news(dic)
+            )
+            # requisition
+            MAIN_URL
+            array
+
+            selected = sorted(
+                # requisition
+                notices.keys(),
+                key=lambda
+                category: (-notices[category], category)
+                )
+
+            # requisition
+            array
+            result = selected
+            MAIN_URL
+
+            return result[:5]
+        except requests.Timeout:
+            return "Error"
+    except requests.Timeout:
+        return None
